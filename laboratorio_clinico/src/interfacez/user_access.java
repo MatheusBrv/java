86268f53019daa13129aeab8b365d43ab2048d97
Pauljiji <Pvargas2818@utm.edu.ec>
2023-09-02 01:40:30 -0500

    Update user_access.java

--- laboratorio_clinico/src/interfacez/user_access.java
+++ laboratorio_clinico/src/interfacez/user_access.java
@@ -11,28 +11,27 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import laboratorio_clinico.conexion_MYSQL;
 
-
 public class user_access extends javax.swing.JFrame {
 
-    //Creacion de metodo para ingresar con un usuario existente en la 
-    //base de datos
+    // Creacion de metodo para ingresar con un usuario existente en la
+    // base de datos
     public void acceso() {
-        conexion_MYSQL c = new conexion_MYSQL();
-        Connection conectar = c.obtener_conexion();
-        int contador = 1;
-        String user = txt_user.getText();
-        String passw = String.valueOf(txt_password.getPassword());
-        //Query para seleciconar un usuario a la base de datos
-        String sql = "SELECT * FROM usuarios WHERE nom_usuario='" + user + "'AND contraseña='" + passw + "'";
+        conexion_MYSQL c = new conexion_MYSQL(); // ta
+        Connection conectar = c.obtener_conexion(); // ta
+        int contador = 1; // ta
+        String user = txt_user.getText(); // ta
+        String passw = String.valueOf(txt_password.getPassword()); // ta
+        // Query para seleciconar un usuario a la base de datos
+        String sql = "SELECT * FROM usuarios WHERE nom_usuario='" + user + "'AND contraseña='" + passw + "'"; // ta
         try {
-            //creamos una conexion a la base de datos
-            Statement st = conectar.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            if (rs.next()) {
+            // creamos una conexion a la base de datos
+            Statement st = conectar.createStatement(); // ta
+            ResultSet rs = st.executeQuery(sql); // ta
+            if (rs.next()) { // tc
 
-                if (contador == 1) {
+                if (contador == 1) { // tc
                     JOptionPane.showMessageDialog(null, "Bienvenido a FAMILY LAB Labortorio clínico.");
-                    Home h = new Home();
+                    Home h = new Home(); // ta
                     h.setVisible(true);
                     this.dispose();
                 }
@@ -44,6 +43,11 @@ public class user_access extends javax.swing.JFrame {
         }
     }
 
+    /*
+     * T= ta+ ta+ ta+ ta+ ta+ta +ta +ta +tc +tc
+     * T= 8ta +2tc
+     */
+
     public user_access() {
         initComponents();
         this.setLocationRelativeTo(null);
@@ -51,9 +55,9 @@ public class user_access extends javax.swing.JFrame {
         setIconImage(icon);
     }
 
-    
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated
+    // Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
@@ -118,33 +122,37 @@ public class user_access extends javax.swing.JFrame {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE,
+                                javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton1ActionPerformed
         dispose();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }// GEN-LAST:event_jButton1ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton2ActionPerformed
         acceso();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }// GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+        // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
+        // (optional) ">
+        /*
+         * If Nimbus (introduced in Java SE 6) is not available, stay with the default
+         * look and feel.
+         * For details see
+         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -170,7 +178,7 @@ public class user_access extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(user_access.class
                     .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
+        // </editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
